a33065017d16c7b83f500ef3ef04f2d0f0e6f2de
mark <mark.korput@gmail.com>
2018-02-28 15:54:45 +0100

    DualStream example app uses ofxHistoryPlot to show average bytes/frame info (from network stream or recording)

--- example-DualStream/src/main.cpp
+++ example-DualStream/src/main.cpp
@@ -2,6 +2,7 @@
 #include "ofMain.h"
 // addons
 #include "ofxOrbbecPersee/ofxOrbbecPersee.h"
+#include "ofxHistoryPlot.h"
 // local
 
 class ofApp : public ofBaseApp{
@@ -38,7 +39,9 @@ class ofApp : public ofBaseApp{
     persee::ReceiverRef receiverRefs[2];
     persee::Recorder recorders[2];
     persee::Playback playbacks[2];
-    persee::Buffer buffers[2];
+    float avgBytesPerFrame[2] = { 0.0f, 0.0f };
+
+    ofxHistoryPlot* plots[2];
 
     // ofEasyCam cam;
     ofTexture textures[2];
@@ -51,18 +54,27 @@ void ofApp::setup() {
 
   receiverRefs[0] = persee::Receiver::createAndStart(src1.address, src1.port);
   receiverRefs[0]->setOutputTo(&recorders[0]);
-  recorders[0].setOutputTo(&buffers[0]);
-  playbacks[0].setOutputTo(&buffers[0]);
+  playbacks[0].setOutputTo(&recorders[0]);
 
   receiverRefs[1] = persee::Receiver::createAndStart(src2.address, src2.port);
   receiverRefs[1]->setOutputTo(&recorders[1]);
-  recorders[1].setOutputTo(&buffers[1]);
-  playbacks[1].setOutputTo(&buffers[1]);
+  playbacks[1].setOutputTo(&recorders[1]);
+
+  plots[0] = new ofxHistoryPlot( NULL, "bytes/frame", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
+  plots[1] = new ofxHistoryPlot( NULL, "bytes/frame", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
+  plots[1]->setColor( ofColor(0,0,255) );
 }
 
 void ofApp::update() {
-  ofxOrbbecPersee::loadDepthTexture(buffers[0], textures[0]);
-  ofxOrbbecPersee::loadDepthTexture(buffers[1], textures[1]);
+  for(int i=0; i<2; i++){
+    auto frameRef = recorders[i].getRef();
+
+    persee::emptyAndInflateBuffer(recorders[i], [this, frameRef, i](const void* data, size_t size){
+      ofxOrbbecPersee::loadDepthTexture(textures[i], data, size);
+      plots[i]->update(frameRef->size()); // write original (compressed) size to our plot
+      this->avgBytesPerFrame[i] = (this->avgBytesPerFrame[i] * 9.0f + frameRef->size()) / 10.0f;
+    });
+  }
 }
 
 void ofApp::draw() {
@@ -72,19 +84,36 @@ void ofApp::draw() {
     textures[0].draw(0, 0);
   }
 
-  ofDrawBitmapString(playbacks[0].isPlaying() ? ofFile(playbacks[0].getFilename()).getBaseName() : src1.address+":"+ofToString(src1.port), 10, 500);
+  std::string speed = ofToString(avgBytesPerFrame[0]) + "bpf - ";
+  std::string source = playbacks[0].isPlaying() ? ofFile(playbacks[0].getFilename()).getBaseName() : src1.address+":"+ofToString(src1.port);
+  ofDrawBitmapString(speed + source, 10, 500);
+  plots[0]->draw(10, 540, 400, 150);
 
   if(textures[1].isAllocated()) {
     textures[1].draw(650, 0);
   }
 
-  ofDrawBitmapString(playbacks[1].isPlaying() ? ofFile(playbacks[1].getFilename()).getBaseName() : src2.address+":"+ofToString(src2.port), 650, 520);
+  speed = ofToString(avgBytesPerFrame[1]) + "bpf - ";
+  source = playbacks[1].isPlaying() ? ofFile(playbacks[1].getFilename()).getBaseName() : src2.address+":"+ofToString(src2.port);
+  ofDrawBitmapString(speed + source, 650, 520);
+  plots[1]->draw(650, 540, 400, 150);
 }
 
 void ofApp::keyPressed(int key) {
-
   // if (key == 'd') { bDrawEdge = !bDrawEdge; }
   // if (key == 'D') { bDrawDepth = !bDrawDepth; }
+  if(key == 'm') {
+    stopPlayback(0);
+    stopPlayback(1);
+    receiverRefs[0]->setOutputTo(NULL);
+    receiverRefs[1]->setOutputTo(NULL);
+  }
+
+  if(key == 'M') {
+    stopPlayback(0);
+    stopPlayback(1);
+  }
+
   if(key == 's') {
     stopPlayback(0);
     stopPlayback(1);
@@ -94,13 +123,13 @@ void ofApp::keyPressed(int key) {
 void ofApp::startPlayback(int which, const std::string& filename) {
   playbacks[which].startThreaded(filename);
   // stop network stream (through recorder)
-  recorders[which].setOutputTo(NULL);
+  receiverRefs[which]->setOutputTo(NULL);
 }
 
 void ofApp::stopPlayback(int which) {
   playbacks[which].stop();
   // reconnect network stream (through recorder)
-  recorders[which].setOutputTo(&buffers[which]);
+  receiverRefs[which]->setOutputTo(&recorders[which]);
 }
 
 void ofApp::dragEvent(ofDragInfo dragInfo) {
