b1da26ffa804c89e5e9e2d5af631d72b064f7b41
mark <mark.korput@gmail.com>
2018-03-01 11:45:29 +0100

    ad kB/s info to DualStream app

--- example-DualStream/src/main.cpp
+++ example-DualStream/src/main.cpp
@@ -44,7 +44,11 @@ class ofApp : public ofBaseApp{
 
     // plot/speed
     float avgBytesPerFrame[2] = { 0.0f, 0.0f };
-    ofxHistoryPlot* plots[2];
+    float avgBytesPerSecond[2] { 0.0f, 0.0f };
+    unsigned int bytesThisSecond[2] { 0,0 };
+    float secondTimer=0;
+
+    ofxHistoryPlot* plots[4];
 
     // gui
     typedef struct {
@@ -57,12 +61,12 @@ class ofApp : public ofBaseApp{
     // ofParameterGroup paramsGui;
     ParsStruct pars[2];
     // ofxPanel gui;
-    bool bDrawGui=true, bDrawHelp=true;
+    bool bDrawGui=true, bDrawHelp=false;
 };
 
 void ofApp::setup() {
   ofSetWindowTitle("ofxOrbbecPersee - DualStream Example");
-  ofSetWindowShape(1290,720);
+  ofSetWindowShape(1290,750);
   ofSetVerticalSync(true);
 
   receiverRefs[0] = persee::Receiver::createAndStart(src1.address, src1.port);
@@ -74,9 +78,18 @@ void ofApp::setup() {
   playbacks[1].setOutputTo(&recorders[1]);
 
   // gui
-  plots[0] = new ofxHistoryPlot( NULL, "bytes/frame", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
-  plots[1] = new ofxHistoryPlot( NULL, "bytes/frame", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
-  plots[1]->setColor( ofColor(0,0,255) );
+  plots[0] = new ofxHistoryPlot( NULL, "kB/frame", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
+  plots[0]->setColor( ofColor(0,255,255) );
+  plots[0]->setRange(0, 400);
+  plots[1] = new ofxHistoryPlot( NULL, "kB/frame", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
+  plots[1]->setColor( ofColor(255,0,255) );
+  plots[1]->setRange(0, 400);
+  plots[2] = new ofxHistoryPlot( NULL, "kB/sec", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
+  plots[2]->setColor( ofColor(0,255,255) );
+  plots[2]->setRange(0, 7000);
+  plots[3] = new ofxHistoryPlot( NULL, "kB/sec", 400, false); //NULL cos we don't want it to auto-update. confirmed by "true"
+  plots[3]->setColor( ofColor(255,0,255) );
+  plots[3]->setRange(0, 7000);
 
   auto cfg = [](ParsStruct& pars) {
     pars.params.add(pars.minDistance.set("minDistance", 50, 0, 15000));
@@ -117,32 +130,54 @@ void ofApp::update() {
           .setMarginRight(this->pars[i].margins[1])
           .setMarginBottom(this->pars[i].margins[2])
           .setMarginLeft(this->pars[i].margins[3])*/);
-      plots[i]->update(frameRef->size()); // write original (compressed) size to our plot
+      plots[i]->update(frameRef->size()/1000.0f); // write original (compressed) size to our plot
       this->avgBytesPerFrame[i] = (this->avgBytesPerFrame[i] * 9.0f + frameRef->size()) / 10.0f;
+      this->bytesThisSecond[i] += frameRef->size();
     });
+  }
+
+  auto t = ofGetElapsedTimeMillis();
+  if(t - secondTimer > 1000.0f) {
+    this->avgBytesPerSecond[0] = ((this->avgBytesPerSecond[0] * 4) + this->bytesThisSecond[0]) / 5.0f;
+    this->avgBytesPerSecond[1] = ((this->avgBytesPerSecond[1] * 4) + this->bytesThisSecond[1]) / 5.0f;
+    secondTimer = t;
+    this->bytesThisSecond[0] = 0;
+    this->bytesThisSecond[1] = 0;
+    plots[2]->update(this->avgBytesPerSecond[0]/1000.0f);
+    plots[3]->update(this->avgBytesPerSecond[1]/1000.0f);
   }
 }
 
 void ofApp::draw() {
   ofBackground(0);
 
-  if(textures[0].isAllocated()) {
-    textures[0].draw(0, 0);
+  { // left
+    if(textures[0].isAllocated()) {
+      textures[0].draw(0, 0);
+    }
+
+    stringstream ss;
+    ss << ofToString(avgBytesPerFrame[0]/1000.0f) + " kB/f - ";
+    ss << ofToString(avgBytesPerSecond[0]/1000.0f) + " kB/s - ";
+    ss << playbacks[0].isPlaying() ? ofFile(playbacks[0].getFilename()).getBaseName() : src1.address+":"+ofToString(src1.port);
+    ofDrawBitmapString(ss.str(), 10, 500);
+    plots[0]->draw(10, 540, 400, 100);
+    plots[2]->draw(10, 640, 400, 100);
   }
 
-  std::string speed = ofToString(avgBytesPerFrame[0]) + "bpf - ";
-  std::string source = playbacks[0].isPlaying() ? ofFile(playbacks[0].getFilename()).getBaseName() : src1.address+":"+ofToString(src1.port);
-  ofDrawBitmapString(speed + source, 10, 500);
-  plots[0]->draw(10, 540, 400, 150);
+  { // right
+    if(textures[1].isAllocated()) {
+      textures[1].draw(650, 0);
+    }
 
-  if(textures[1].isAllocated()) {
-    textures[1].draw(650, 0);
+    stringstream ss;
+    ss << ofToString(avgBytesPerFrame[1]/1000.0f) + " kB/f - ";
+    ss << ofToString(avgBytesPerSecond[1]/1000.0f) + " kB/s - ";
+    ss << playbacks[1].isPlaying() ? ofFile(playbacks[1].getFilename()).getBaseName() : src2.address+":"+ofToString(src2.port);
+    ofDrawBitmapString(ss.str(), 650, 520);
+    plots[1]->draw(650, 540, 400, 100);
+    plots[3]->draw(650, 640, 400, 100);
   }
-
-  speed = ofToString(avgBytesPerFrame[1]) + "bpf - ";
-  source = playbacks[1].isPlaying() ? ofFile(playbacks[1].getFilename()).getBaseName() : src2.address+":"+ofToString(src2.port);
-  ofDrawBitmapString(speed + source, 650, 520);
-  plots[1]->draw(650, 540, 400, 150);
 
   if(bDrawGui) {
     pars[0].gui.draw();
