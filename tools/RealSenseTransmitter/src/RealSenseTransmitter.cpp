f76f1b878c81be7822b93860aa89a95c642d1867
mark <mark.korput@gmail.com>
2018-06-12 10:44:34 +0200

    building (compiling AND linking) dumbed down version of RealSense example app

--- tools/RealSenseTransmitter/src/RealSenseTransmitter.cpp
+++ tools/RealSenseTransmitter/src/RealSenseTransmitter.cpp
@@ -2,7 +2,7 @@
 // Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 
 #include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
-#include "example.hpp"          // Include short list of convenience functions for rendering
+// #include "example.hpp"          // Include short list of convenience functions for rendering
 
 #include "../../../libs/DepthStream/src/TransmitterAgent.h"
 
@@ -11,20 +11,20 @@
 int main(int argc, char * argv[]) try
 {
     // Create a simple OpenGL window for rendering:
-    window app(1280, 720, "RealSense Capture Example");
+    // window app(1280, 720, "RealSense Capture Example");
     // Declare two textures on the GPU, one for color and one for depth
-    texture depth_image, color_image;
+    // texture depth_image, color_image;
 
     // Declare depth colorizer for pretty visualization of depth data
-    rs2::colorizer color_map;
+    // rs2::colorizer color_map;
 
     // Declare RealSense pipeline, encapsulating the actual device and sensors
     rs2::pipeline pipe;
     // Start streaming with default recommended configuration
     pipe.start();
 
-    while(app) // Application still alive?
-    {
+    // while(app) // Application still alive?
+    // {
         rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
 
         // rs2::frame depth = color_map(data.get_depth_frame()); // Find and colorize the depth data
@@ -32,9 +32,9 @@ int main(int argc, char * argv[]) try
         rs2::frame color = data.get_color_frame();            // Find the color data
 
         // Render depth on to the first half of the screen and color on to the second
-        depth_image.render(depth, { 0,               0, app.width() / 2, app.height() });
-        color_image.render(color, { app.width() / 2, 0, app.width() / 2, app.height() });
-    }
+        // depth_image.render(depth, { 0,               0, app.width() / 2, app.height() });
+        // color_image.render(color, { app.width() / 2, 0, app.width() / 2, app.height() });
+    // }
 
     return EXIT_SUCCESS;
 }
