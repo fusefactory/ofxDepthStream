eea319db7863018c63b93ba167bfdfa5504c63f6
mark <mark.korput@gmail.com>
2018-06-12 10:21:33 +0200

    copied code CMakeLists.txt from RealSense capture example app (not building yet)

---
+++ tools/RealSenseTransmitter/README.md
@@ -0,0 +1,4 @@
+
+Follow installation instructions;
+https://realsense.intel.com/sdk-2/#install
+sudo xcode-select --reset
---
+++ tools/RealSenseTransmitter/src/RealSenseTransmitter.cpp
@@ -0,0 +1,50 @@
+// License: Apache 2.0. See LICENSE file in root directory.
+// Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+#include <librealsense2/rs.hpp> // Include RealSense Cross Platform API
+#include "example.hpp"          // Include short list of convenience functions for rendering
+
+#include "../../../libs/DepthStream/src/TransmitterAgent.h"
+
+// Capture Example demonstrates how to
+// capture depth and color video streams and render them to the screen
+int main(int argc, char * argv[]) try
+{
+    // Create a simple OpenGL window for rendering:
+    // window app(1280, 720, "RealSense Capture Example");
+    // Declare two textures on the GPU, one for color and one for depth
+    texture depth_image, color_image;
+
+    // Declare depth colorizer for pretty visualization of depth data
+    rs2::colorizer color_map;
+
+    // Declare RealSense pipeline, encapsulating the actual device and sensors
+    rs2::pipeline pipe;
+    // Start streaming with default recommended configuration
+    pipe.start();
+
+    while(app) // Application still alive?
+    {
+        rs2::frameset data = pipe.wait_for_frames(); // Wait for next set of frames from the camera
+
+        // rs2::frame depth = color_map(data.get_depth_frame()); // Find and colorize the depth data
+        rs2::frame depth = data.get_depth_frame(); // Find and colorize the depth data
+        rs2::frame color = data.get_color_frame();            // Find the color data
+
+        // Render depth on to the first half of the screen and color on to the second
+        depth_image.render(depth, { 0,               0, app.width() / 2, app.height() });
+        color_image.render(color, { app.width() / 2, 0, app.width() / 2, app.height() });
+    }
+
+    return EXIT_SUCCESS;
+}
+catch (const rs2::error & e)
+{
+    std::cerr << "RealSense error calling " << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
+catch (const std::exception& e)
+{
+    std::cerr << e.what() << std::endl;
+    return EXIT_FAILURE;
+}
